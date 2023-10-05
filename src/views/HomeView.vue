d02447fbc409d0fd9d5c1396c5283b7ca8ea8daf
jhon-crypt <johnoladele690@gmail.com>
2023-10-05 18:49:31 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from '../components/header/header.vue'
+import HeadingHero from '../components/hero/HeadingHero.vue'
+import FeaturesSection from '../components/section/FeaturesSection.vue'
+import SecondFeatures from '../components/section/SecondFeatures.vue'
+import ThirdFeaturesSection from '../components/section/ThirdFeaturesSection.vue'
+import GallerySection from '../components/section/GallerySection.vue'
+</script>
+
+<template>
+  <main class="px-6">
+    <Header />
+    <HeadingHero />
+    <FeaturesSection />
+    <SecondFeatures />
+    <ThirdFeaturesSection />
+    <GallerySection />
+  </main>
+</template>
