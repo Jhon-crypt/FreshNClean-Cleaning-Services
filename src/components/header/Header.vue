ec6957ee8485f37faa6bb33b8be90134b6d90ba4
jhon-crypt <johnoladele690@gmail.com>
2023-10-06 01:26:38 +0100

    Updated ui components

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -21,7 +21,7 @@
                     <li><a>Item 3</a></li>
                 </ul>
             </div>
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <a className="btn btn-ghost normal-case text-xl">FreshNd-Clean-Services</a>
         </div>
         <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,15 +5,21 @@ import FeaturesSection from '../components/section/FeaturesSection.vue'
 import SecondFeatures from '../components/section/SecondFeatures.vue'
 import ThirdFeaturesSection from '../components/section/ThirdFeaturesSection.vue'
 import GallerySection from '../components/section/GallerySection.vue'
+import ContactSection from '../components/section/contactSection.vue'
+import Footer from '../components/footer/Footer.vue'
 </script>
 
 <template>
-  <main class="px-6">
+  <main>
     <Header />
-    <HeadingHero />
-    <FeaturesSection />
-    <SecondFeatures />
-    <ThirdFeaturesSection />
-    <GallerySection />
+    <main class="px-2">
+      <HeadingHero />
+      <FeaturesSection />
+      <SecondFeatures />
+      <ThirdFeaturesSection />
+      <GallerySection />
+      <ContactSection />
+    </main>
+    <Footer />
   </main>
 </template>
